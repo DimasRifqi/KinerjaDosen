a6f10e96d99562549c62e326d8a5b05a3077d887
Fatimah Nur Alifiah <fatimahnalfh@gmail.com>
2024-10-04 14:20:03 +0700

    rework view login to match template

--- resources/views/layouts/auth/common-head.blade.php
+++ resources/views/layouts/auth/common-head.blade.php
@@ -14,3 +14,17 @@
 <!-- Scripts -->
 @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 <!-- end LaravelUI -->
+
+<link rel="stylesheet" href="{{ asset('staradmin/vendors/feather/feather.css') }}">
+<link rel="stylesheet" href="{{ asset('staradmin/vendors/mdi/css/materialdesignicons.min.css') }}">
+<link rel="stylesheet" href="{{ asset('staradmin/vendors/ti-icons/css/themify-icons.css') }}">
+<link rel="stylesheet" href="{{ asset('staradmin/vendors/typicons/typicons.css') }}">
+<link rel="stylesheet" href="{{ asset('staradmin/vendors/simple-line-icons/css/simple-line-icons.css') }}">
+<link rel="stylesheet" href="{{ asset('staradmin/vendors/css/vendor.bundle.base.css') }}">
+<!-- endinject -->
+<!-- Plugin css for this page -->
+<!-- End plugin css for this page -->
+<!-- inject:css -->
+<link rel="stylesheet" href="{{ asset('staradmin/css/vertical-layout-light/style.css') }}">
+<!-- endinject -->
+<link rel="shortcut icon" href="{{ asset('staradmin/images/favicon.png') }}"/>

f3236963cc2e5f7d3f2306905c06f92fa787de19
DimasRifqi <dimasrifqiramadhani@gmail.com>
2024-10-16 08:58:12 +0700

    update ajax pagination univ

---
+++ resources/views/home/anggota/komponen/data_kosong.blade.php
@@ -0,0 +1,7 @@
+<div class="alert alert-warning text-center">
+    <strong>Tidak Ada Data</strong>
+</div>
+
+<div class="text-center">
+    <a href="{{ route('univ.index') }}" class="btn btn-danger mt-3">Back</a>
+</div>
